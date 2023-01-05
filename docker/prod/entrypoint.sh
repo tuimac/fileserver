1042c7352d78a8c45baf4fea77153b904b881141
tuimac <tuimac.devadm01@gmail.com>
2023-01-05 23:50:42 +0900

    save

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-BASE_DIR='/root'
+function config_variable(){
+    if [ $# -ne 2 ]; then
+        echo 'Need the argument which are PJT_NAME and WORK_DIR.'
+        exit 1
+    fi
+    BACKEND_DIR=$1
+}
 
-cd $BASE_DIR
-if [ -e magic_packet ]; then
-    cd magic_packet
-    git pull
-else
-    git clone https://github.com/tuimac/magic_packet
-fi
+function start_backend(){
+    cd ${BACKEND_DIR}
+    python3 manage.py runserver 0:8000 &
+}
 
-cd ${BASE_DIR}/magic_packet/src/frontend
-npm ci
-npm start &
+function start_nginx(){
+    /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
+}
 
-cd $BASE_DIR/magic_packet/src/backend
-gunicorn -c /etc/gunicorn/gunicorn.conf.py &
+function main(){
+    start_backend
+    start_nginx
+}
 
-/usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
+
+main $1
